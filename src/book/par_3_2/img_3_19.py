f17e218d4a03924f5e6ea18ac59bf50768ae2836
Andrii Krenevych <krenevych@knu.ua>
2026-04-01 08:12:23 +0300

    Image for book

--- src/book/par_3_2/img_3_19.py
+++ src/book/par_3_2/img_3_19.py
@@ -15,34 +15,52 @@ if __name__ == '__main__':
         axis_show=False,  # чи показувати осі координат
     )
 
+    orig = 0, 0
+    x_dir = 3.1, 0
     z_dir = -1.4, -1.2
-
+    y_dir = 0, 2.4
     v_dir = 1.6, 0.9
-    v_proj_vert =  1.75, 0.95, 1.7, -0.8
+    v_arrow = 1.75, 0.95
+    v_proj_vert =  1.7, -0.8
     v_proj_oxy_ccords =  1.7, -0.8
     v_proj_z_coord = -0.9, -0.8
     v_proj_x_coord = 2.8, 0.0
+    v_proj_y_coord =  (0.0, 0.8 + 0.9 + 0.15)
 
-    Ox = Axis(3.1, 0, color="red", linewidth=1.5, label="$x$", label_offset=(0.1, 0.1))
-    Oy = Axis(0, 2.4, color="green", linewidth=1.5, label="$y$", label_offset=(0.1, 0.2))
+    Ox = Axis(x_dir, color="red", linewidth=1.5, label="$x$", label_offset=(0.1, 0.1))
+    Oy = Axis(y_dir, color="green", linewidth=1.5, label="$y$", label_offset=(0.1, 0.2))
     Oz = Axis(z_dir, color="blue", linewidth=1.5, label="$z$", label_offset=(-0.35, -0.3))
     v = Axis(v_dir, color="brown", linewidth=1.5, label="$v$", label_offset=(0.1, 0.2))
-    v_proj_v = LineModel(*v_proj_vert, color="black", linewidth=1., line_style="--", )
+    v_proj_v = LineModel(v_arrow, v_proj_vert, color="black", linewidth=1., line_style="--", )
     v_proj_oxy = LineModel((0, 0), v_proj_oxy_ccords, color="black", linewidth=1., line_style="--", )
     v_proj_z = LineModel(v_proj_oxy_ccords, v_proj_z_coord, color="black", linewidth=1., line_style="--", )
     v_proj_x = LineModel(v_proj_oxy_ccords, v_proj_x_coord, color="black", linewidth=1., line_style="--", )
+    v_proj_y = LineModel(v_arrow, v_proj_y_coord, color="black", linewidth=1., line_style="--", )
 
     def frame1(scene: Scene):
-        draw_arc((0, 0), z_dir, v_proj_oxy_ccords,
+        draw_arc(orig, z_dir, v_proj_oxy_ccords,
                  radius=0.25,
-                 color="red",
+                 color="blue",
                  linestyle="--",
                  linewidth=2.0,
                  )
 
-        print_label(start= (-0.1, -0.3),
+        print_label(start= orig,
                     label=r"$\varphi$",
-                    label_offset=(0.0, -0.2),
+                    label_offset=(-0.1, -0.5),
+                    label_fontsize=LABEL_FONT_SIZE,
+                    )
+
+        draw_arc(orig, y_dir, v_dir,
+                 radius=0.35,
+                 color="green",
+                 linestyle="--",
+                 linewidth=2.0,
+                 )
+
+        print_label(start= orig,
+                    label=r"$\theta$",
+                    label_offset=(0.1, 0.45),
                     label_fontsize=LABEL_FONT_SIZE,
                     )
 
@@ -54,6 +72,7 @@ if __name__ == '__main__':
     simple_scene["v_proj_oxy"] = v_proj_oxy
     simple_scene["v_proj_z"] = v_proj_z
     simple_scene["v_proj_x"] = v_proj_x
+    simple_scene["v_proj_y"] = v_proj_y
 
     simple_scene.add_frames(frame1)
 
